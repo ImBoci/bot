f2a46a5935ff7956b00781e03987ea0407c2d340
ImBoci <31576943+ImBoci@users.noreply.github.com>
2018-08-09 19:56:37 +0200

    Add files via upload

---
+++ commands/reboot.js
@@ -0,0 +1,24 @@
+const settings = require ("../botsettings.json")
+
+exports.run = async (client, message, args, level) => {// eslint-disable-line no-unused-vars
+    if(message.author.id != settings.ownerID) return message.reply("Only the owner can use this command.");
+    await message.reply("Bot is shutting down.");
+    client.commands.forEach( async cmd => {
+      await client.unloadCommand(cmd);
+    });
+    process.exit(1);
+  };
+  
+  exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: "Bot Admin"
+  };
+  
+  exports.help = {
+    name: "reboot",
+    category: "System",
+    description: "Shuts down the bot. If running under PM2, bot will restart automatically.",
+    usage: "!reboot"
+  };
